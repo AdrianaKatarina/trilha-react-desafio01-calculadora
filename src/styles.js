9cded190f345833afb17980dd38dc29e32858cc1
AdrianaKatarina <dricakatarina@gmail.com>
2023-09-20 12:41:40 -0300

    style: Melhorando a responsividade

--- src/styles.js
+++ src/styles.js
@@ -1,32 +1,32 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    background-color: #CACACA;
+  width: 100vw;
+  height: 100vh;
+  background-color: #CACACA;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 
 export const Content = styled.div`
-    background-color: #FFFFFF;
-    width: 50%;
+  background-color: #FFFFFF;
+  max-width: 224px;
 `
 
 export const Row = styled.div`
-    display:flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
+  display:flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `
 
 export const Column = styled.div`
-    display:flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
+  display:flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `
 
